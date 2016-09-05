8e588bd85ce928239b740d70cda1d4f07b9b9498
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-09-05 15:33:04 +0800

    cleaned up indentations

--- code-style.rb
+++ code-style.rb
@@ -2,27 +2,27 @@ class Guessing_game
   VALID_Numbers = (1..100).to_a # Store valid answers in an array
 
   def initialize answer; @answer = answer
-@solved = false
-# Validate input
-  raise "Answer must be between 1 and 100" unless VALID_Numbers.include? @answer
+    @solved = false
+  # Validate input
+    raise "Answer must be between 1 and 100" unless VALID_Numbers.include? @answer
   end
   
-def guess ( number )
-if number == @answer # Check if the two are equal
-@solved = true
+  def guess ( number )
+    if number == @answer # Check if the two are equal
+      @solved = true
       :correct
-  elsif (number > @answer) # Check if the guess is higher
-@solved = false
-    return :high
-elsif(number<@answer) # Check if the guess is lower
+    elsif (number > @answer) # Check if the guess is higher
       @solved = false
-    :low
-end
+      return :high
+    elsif(number<@answer) # Check if the guess is lower
+      @solved = false
+      :low
+    end
   end
   
   def solved?
-  @solved
-end
+    @solved
+  end
 end
 
 game = Guessing_game.new(10)
@@ -38,6 +38,6 @@ puts game.solved?   == false
 
 begin
   Guessing_game.new(200)
-rescue RuntimeError => e
+  rescue RuntimeError => e
   puts e.to_s == "Answer must be between 1 and 100"
 end
